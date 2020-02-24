c13d5d2896f3095c43862e0be7a079f56b1eb528
icaste <ignacio.castillejos@nike.com>
2020-02-23 21:42:14 -0800

    Adding controller unit tests

--- catalog-web-app/src/test/java/com/ic/boot/cat/catalogwebapp/CatalogWebAppApplicationTests.java
+++ catalog-web-app/src/test/java/com/ic/boot/cat/catalogwebapp/CatalogWebAppApplicationTests.java
@@ -1,13 +1,58 @@
 package com.ic.boot.cat.catalogwebapp;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ic.boot.cat.catalogwebapp.domain.CatalogDto;
+import com.ic.boot.cat.catalogwebapp.service.CatalogService;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@SpringBootTest
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest
 class CatalogWebAppApplicationTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CatalogService catalogService;
 
     @Test
-    void contextLoads() {
+    void testPutCatalog() throws Exception {
+        CatalogDto catalogDto = new CatalogDto("a-name", "US");
+        String catalodDtoJson = new ObjectMapper().writeValueAsString(catalogDto);
+        doNothing().when(catalogService).updateCatalog(any(CatalogDto.class));
+
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.put("/v1/catalog/newshoes")
+                        .content(catalodDtoJson)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").doesNotExist());
     }
 
+    @Test
+    void testDeleteCatalog() throws Exception {
+        doNothing().when(catalogService).deleteCatalog(anyString());
+
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.delete("/v1/catalog/newshoes")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isNoContent())
+                .andExpect(jsonPath("$").doesNotExist());
+    }
 }
