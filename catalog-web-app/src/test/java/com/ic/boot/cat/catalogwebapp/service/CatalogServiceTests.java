57b67d1f30a0309839d626deac0211a081667aac
icaste <ignacio.castillejos@nike.com>
2020-03-02 22:53:52 -0800

    Added test

--- catalog-web-app/src/test/java/com/ic/boot/cat/catalogwebapp/service/CatalogServiceTests.java
+++ catalog-web-app/src/test/java/com/ic/boot/cat/catalogwebapp/service/CatalogServiceTests.java
@@ -1,19 +1,32 @@
 package com.ic.boot.cat.catalogwebapp.service;
 
+import com.ic.boot.cat.catalogwebapp.data.Catalog;
 import com.ic.boot.cat.catalogwebapp.data.CatalogRepository;
+import com.ic.boot.cat.catalogwebapp.data.Product;
 import com.ic.boot.cat.catalogwebapp.data.ProductRepository;
 import com.ic.boot.cat.catalogwebapp.domain.CatalogDto;
+import com.ic.boot.cat.catalogwebapp.domain.LocalizedCatalogDto;
+import com.ic.boot.cat.catalogwebapp.domain.ProductDto;
 import com.ic.boot.cat.catalogwebapp.exception.CatalogAlreadyExistsException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.ArgumentMatchers;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
@@ -28,6 +41,12 @@ public class CatalogServiceTests {
 
     @Mock
     private ProductRepository productRepository;
+
+    @Captor
+    private ArgumentCaptor<Iterable<Product>> productIterableCaptor;
+
+    @Captor
+    private ArgumentCaptor<Catalog> catalogCaptor;
 
     private CatalogService catalogService;
 
@@ -79,6 +98,44 @@ public class CatalogServiceTests {
         spy.updateCatalog(catalogDto);
 
         verify(spy, times(1)).upsertCatalog(catalogDto);
+    }
+
+    @Test
+    void upsertCatalog() {
+        CatalogDto catalogDto = new CatalogDto("a-name", "US");
+
+        LocalizedCatalogDto localizedCatalogDto = new LocalizedCatalogDto("en");
+        ProductDto productDto1 = new ProductDto("name1", "desc1");
+        ProductDto productDto2 = new ProductDto("name2", "desc2");
+
+        List<ProductDto> productDtos = Arrays.asList(productDto1, productDto2);
+        localizedCatalogDto.setProducts(productDtos);
+        catalogDto.addLocalizedCatalog(localizedCatalogDto);
+
+        // Returned values are irrelevant for the method and test
+        when(catalogRepository.save(any(Catalog.class))).thenReturn(null);
+        when(productRepository.saveAll(anyList())).thenReturn(null);
+
+        catalogService.upsertCatalog(catalogDto);
+
+        verify(catalogRepository, times(1)).save(catalogCaptor.capture());
+        Catalog catalogCaptorValue = catalogCaptor.getValue();
+        assertEquals(catalogDto.getName(), catalogCaptorValue.getName());
+        assertEquals(catalogDto.getCountry(), catalogCaptorValue.getCountry());
+
+        verify(productRepository, times(1)).saveAll(productIterableCaptor.capture());
+        Iterable<Product> productIterableCaptorValue = productIterableCaptor.getValue();
+        List<Product> result = new ArrayList<>();
+        productIterableCaptorValue.forEach(result::add);
+        assertEquals(2, result.size());
+
+        assertTrue(result.stream().anyMatch(productPredicate(productDto1)));
+        assertTrue(result.stream().anyMatch(productPredicate(productDto2)));
+    }
+
+    private static Predicate<Product> productPredicate(ProductDto productDto) {
+        return product -> product.getName().equals(productDto.getName()) &&
+                product.getDescription().equals(productDto.getDescription());
     }
 
     @Test
